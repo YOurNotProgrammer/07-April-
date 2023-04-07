2a68e8626f5842dbd72aa41a39b75baa4243eed9
yournotprogrammer <xkr403@gmail.com>
2023-04-07 09:52:43 +0900

    delete useless code

--- Lotto.cpp
+++ Lotto.cpp
@@ -9,32 +9,24 @@ int main()
 {
 	srand((unsigned int)time(NULL));
 
-	//random_device rd;
-	//mt19937 mt(rd());
-	//uniform_int_distribution<> lotto(1, 45);
-
-	//cout << lotto(mt) << endl;
-	int count = 0;
-	int lotto[45];
+	int Count = 0;
+	int Lotto[45];
 
 	for (int i = 0; i < 45; i++) 
 	{
-		lotto[i] = i + 1;
+		Lotto[i] = i + 1;
 	}
 
 	do 
 	{
 		int Num = rand() % 45;
-		if (lotto[Num] != 0) 
+		if (Lotto[Num] != 0) 
 		{
-			cout << lotto[Num] << endl;
-			lotto[Num] = 0;
-			count += 1;
+			cout << Lotto[Num] << endl;
+			Lotto[Num] = 0;
+			Count += 1;
 		} 
-		else
-		{
-		}
-	} while (count != 6);
+	} while (Count != 6);
 
 	return 0;
 }
