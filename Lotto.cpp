0b168f4be358637b003bfd2ab43a2cb8b0add0b1
yournotprogrammer <xkr403@gmail.com>
2023-04-07 09:49:52 +0900

    random num 6

--- Lotto.cpp
+++ Lotto.cpp
@@ -5,29 +5,36 @@
 
 using namespace std;
 
-int main() 
+int main()
 {
-	//srand((unsigned int)time(NULL));
+	srand((unsigned int)time(NULL));
 
-	//int a = rand() % 46;
-	//int b = rand() % 46;
-	//int c = rand() % 46;
-	//int d = rand() % 46;
-	//int e = rand() % 46;
-	//int f = rand() % 46;
+	//random_device rd;
+	//mt19937 mt(rd());
+	//uniform_int_distribution<> lotto(1, 45);
 
-	//cout << a << endl << b << endl << c << endl << d << endl << e << endl << f;
+	//cout << lotto(mt) << endl;
+	int count = 0;
+	int lotto[45];
 
-	random_device rd;
-	mt19937 mt(rd());
-	uniform_int_distribution<> lotto(1, 45);
+	for (int i = 0; i < 45; i++) 
+	{
+		lotto[i] = i + 1;
+	}
 
-	cout << lotto(mt) << endl;
-	cout << lotto(mt) << endl;
-	cout << lotto(mt) << endl;
-	cout << lotto(mt) << endl;
-	cout << lotto(mt) << endl;
-	cout << lotto(mt) << endl;
+	do 
+	{
+		int Num = rand() % 45;
+		if (lotto[Num] != 0) 
+		{
+			cout << lotto[Num] << endl;
+			lotto[Num] = 0;
+			count += 1;
+		} 
+		else
+		{
+		}
+	} while (count != 6);
 
 	return 0;
 }
